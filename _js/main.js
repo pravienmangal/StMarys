c36568778f87d47462a7e3aa47df496242afb93f
unknown <billgajen@gmail.com>
2016-03-04 12:16:43 +0000

    minor changes to style

--- _js/main.js
+++ _js/main.js
@@ -21,190 +21,91 @@ $(function(){
 			}
 		}
 
-		var cookieWarning = function() { //-------Private Method
-			$('.cookie-warning .button').on('click', function(e) {
-				e.preventDefault();
-				$('.cookie-warning').slideUp();
-				$.cookie("SavillsIMCookieWarning", "SavillsIM-02022016CW", { expires: 28 });
-			});
-
-			if (!$.cookie('SavillsIMCookieWarning')) {
-				setTimeout(function(){ 
-					$('.cookie-warning').slideToggle();
-				}, 2000);
-			}
-		}
-
 		var general = function () { //-------Public Method
 
-			// Calling Private Method
-			cookieWarning();
+			// bxSlider carousels
+			$('.bxslider').bxSlider();
+			$('.event-carousel').bxSlider();
+			$('.isDesktop .news-slider').bxSlider();
+			$('.isDesktop .highlight-slider').bxSlider();
 
-			// Mobile menu
-			var mainNav = $('.main-nav'),
-				modalOverlay = $('.modal-overlay');
-
-			$('.mobile-nav').on('click', function() {
-				$(this).toggleClass('selected');
-				mainNav.toggle();
-				modalOverlay.toggle();
+			// Ministry navigation slide
+			$('.main-nav ul li').on('click', function(e) {
+				if($(this).hasClass('ministry')){
+					e.preventDefault();
+					$('.ministry-nav').slideDown(500);
+					$('nav li a').removeClass('selected');
+					$('a', this).addClass('selected');
+				} else {
+				$('.ministry-nav').slideUp(500);
+				}
 			});
 
-			// Card slide open
-			$('.toggle-btn').on('click', function() {
-				var container = $(this).parent();
+			// Close navigation slider
+			$('.ministry-nav .icon-cancel').on('click', function(){
+				$('.ministry-nav').slideUp(500);
+			});	
 
-				$('.toggle-content', container).toggle();
-				$('.icon', this).toggleClass('icon-chevron-down icon-chevron-up');
+
+			// Ministry links and contents
+			var $ministryLink = $('.ministry-items li a');
+
+			$ministryLink.click(function(e){
+				e.preventDefault();
+
+				//To show clicked link as active
+				$('.ministry-items li a.selected').removeClass('selected');
+				$(this).addClass('selected');
+				
+				//Which content to show			
+				var $contentToShow = $(this).attr('data-contentId');
+
+				//Which content to hide
+				$('.ministry-content-item.active').hide(function(){
+					$(this).removeClass('active');
+					$('#'+ $contentToShow).show(function(){
+						$(this).addClass('active');
+					});
+				});
 			});
 
-			// Sticky header 
+
+			// Click view calendar event - add class
+			$('.view-calendar').on('click', function(e) {
+				e.preventDefault();
+				$('.events-calendar').toggleClass('selected');
+				scrollToTop();
+			});
+
+			// Closing the calender slide
+			$('.icon-cancel').on('click', function(){
+				$('.events-calendar').removeClass('selected');
+			});
+
+			// Click view event block
+			$('.event-block').on('click', function() {
+				//On-click add color to the date slide event content
+				$(this).toggleClass('clicked');
+				$('.event-content', this).slideToggle();
+			});
+
+			// Scroll posioning on sticky header
 			$(window).scroll(function(){
-				if ($(window).scrollTop() > 40) {
-					$('.isDesktop .logo-nav-search').addClass('sticky');
-					$('.isDesktop .header-container').css('margin-bottom', '65px');
+				if($(window).scrollTop() >= 42) {
+					$('.isDesktop header').addClass('fixed');
 				} else {
-					$('.isDesktop .logo-nav-search').removeClass('sticky');
-					$('.isDesktop .header-container').css('margin-bottom', '0');
+					$('.isDesktop header').removeClass('fixed');
 				}
 			});
 
-			// Infographic carousel
-			var maxSlides,
-				slideWidth,
-				width = $(window).width();
-
-			if (width < 640) {
-				maxSlides = 1;
-				slideWidth = 480;
-			} else if (width < 1224) {
-				maxSlides = 3;
-				slideWidth = 312;
-			} else {
-				maxSlides = 3;
-				slideWidth = 356;
+			function scrollToTop() {
+				$("html, body").animate({ scrollTop: 0 }, 600);
 			}
-
-			$('.slider1').bxSlider({
-				slideWidth: slideWidth,
-				minSlides: 1,
-				maxSlides: maxSlides
+			// Scroll to top
+			$('.back-to-top').click(function(e){
+				e.preventDefault();
+				scrollToTop()
 			});
-
-			// Article share
-			$('.share-item a').click(function(){
-				window.open(this.href, 'Share', "width=600, height=600");
-				return false;
-			});
-
-			$('.share-item a').each(function() {
-				var _href = $(this).attr('href'),
-				currentLoc = window.location.href,
-				title = encodeURIComponent($('h1').text());
-
-				if ($(this).parent().hasClass('twitter')) {
-					$(this).attr('href', _href + currentLoc + '&text=' + title + ':' + '&url=' + currentLoc +  '&via=SavillsIM');
-				} else if ($(this).parent().hasClass('linkedin')) {
-					$(this).attr('href', _href  + currentLoc +'&title='+ title);
-				} else {
-					$(this).attr("href", _href + currentLoc);
-				}
-			});
-
-			//funds selector
-			$('#funds-selector').on('change', function() {
-				if($(this).val() === 'open') {
-					$('.closed-fund').hide();
-					$('.open-fund').show();
-				} else {
-					$('.open-fund').hide();
-					$('.closed-fund').show();
-				}
-			});
-
-			//Search toggle
-			$('.search .icon-search').on('click', function() {
-				$('.search-form').toggle();
-				$('.search-form input').focus();
-			});
-			$(document).keyup(function(e) {
-				if(e.keyCode == 27) {
-					$('.search-form').hide();
-				}
-			});
-
-			//apend / remove search field
-			if (!html.hasClass('isDesktop')) {
-				$('.main-nav').prepend($('.search-form form'));
-			}
-
-			//location selector
-			function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
-				var R = 6371; // Radius of the earth in km
-				var dLat = deg2rad(lat2-lat1);  // deg2rad below
-				var dLon = deg2rad(lon2-lon1); 
-				var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon/2) * Math.sin(dLon/2); 
-				var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-				var d = R * c; // Distance in km
-				return d;
-			}
-
-			function deg2rad(deg) {
-				return deg * (Math.PI/180)
-			}
-
-			function sortLocation(position) {
-				var lat, long, sortFunction;
-				lat = position.coords.latitude;
-				long = position.coords.longitude;
-				
-				sortFunction = function(a, b) {
-					var aLat, aLong, bLat, bLong, aDistance, bDistance;
-					aLat = a.getAttribute("data-lat");
-					aLong = a.getAttribute("data-long");
-					bLat = b.getAttribute("data-lat");
-					bLong = b.getAttribute("data-long");
-
-					aDistance = getDistanceFromLatLonInKm(lat, long, aLat, aLong)
-					bDistance = getDistanceFromLatLonInKm(lat, long, bLat, bLong)
-					return parseFloat(aDistance) - parseFloat(bDistance);
-				}
-
-				var selectItems = $('.location-select-item').get();
-				selectItems.sort(sortFunction);
-
-				for (var i = 0; i < selectItems.length; i++) {
-					selectItems[i].parentNode.appendChild(selectItems[i]);
-				}
-
-				var items = $('.location-item').get();
-				items.sort(sortFunction);
-				
-				for (var i = 0; i < items.length; i++) {
-					items[i].parentNode.appendChild(items[i]);
-				}
-
-			}
-
-			function getLocation() {
-				if (navigator.geolocation) {
-					navigator.geolocation.getCurrentPosition(sortLocation);
-				} else { 
-					console.log("Geolocation is not supported by this browser.");
-				}
-			}
-			getLocation();
-
-			$('#location-selector').on('change', function() {
-				if(isMobile) {
-					$('.location-item').hide();
-					$('.location-' + $(this).val()).show();
-				}
-				else {
-					$('.location-item').show();
-				}
-			});
-			$('#location-selector').trigger('change');
 		}
 
 		return {
@@ -222,12 +123,6 @@ $(function(){
 		var html = $('html');
 
 		Module.deviceClass();
-		//apend / remove search field
-		if (!html.hasClass('isDesktop')) {
-			$('.main-nav').prepend($('.search-form form'));
-		} else {
-			$('.search-form').append($('.main-nav form'));
-		}
 	});
 
 });
